8e8c4a567ad40fd1588999b744fe5c0c6fc98b4b
piersfinlayson <piersfinlayson@users.noreply.github.com>
2025-10-11 11:08:57 +0000

    deploy: 2495dcacf89cc5df542a5c4df6074948545dee2a

--- releases/v0.1.1/js/footer.js
+++ releases/v0.1.1/js/footer.js
@@ -1,3 +1,3 @@
-            fetch('https://onerom.org/footer.html')
-                .then(response => response.text())
-                .then(data => document.getElementById('footer').innerHTML = data);
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data);
